445489668cf8e811b4b059ee72d9c81d72c604e1
claudio_hernandez <claudio.hernandez.ing@gmail.com>
2024-11-20 18:46:01 -0300

    Roadmap: update: lists

---
+++ 00-roadmap/00-basics/xx-example-codes/0.6.5-list-errors.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+motorcycles = ["honda", "yamaha", "suzuki"]
+print(motorcycles[3])
+
+motorcycles = ["honda", "yamaha", "suzuki"]
+print(motorcycles[-1])
+
+motorcycles = []
+print(motorcycles[-1])
